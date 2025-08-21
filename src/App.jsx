94473f4a99689ac8ac397b2457a0999ea49270f5
H C Teo <sukasuka@gmail.com>
2025-08-21 19:44:44 +0800

    Test 1 for CI workflow

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import './App.css';
-import ProductPage from './components/ProductPage';
+import "./App.css";
+import ProductPage from "./components/ProductPage";
 
 function App() {
-  
+  const unusedVariable = "Why am I here?";
   return (
     <>
       <ProductPage />
-      
     </>
-  )
+  );
 }
 
 export default App;
